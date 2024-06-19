10c1c0f54565df628b7131c5273ac5b9932426af
xiang <xiang@tensoropera.com>
2024-06-19 03:55:57 +0000

    modify test cases

--- python/tests/test_deploy/test_deploy.py
+++ python/tests/test_deploy/test_deploy.py
@@ -1,12 +1,11 @@
 import os.path
 import time
 import fedml
+
+api_key = os.environ.get('API_KEY')
 # Login
-fedml.set_env_version("test")
 fedml.set_local_on_premise_platform_port(18080)
-error_code, error_msg = fedml.api.fedml_login(api_key="")
-if error_code != 0:
-    raise Exception("API Key is invalid!")
+error_code, error_msg = fedml.api.fedml_login(api_key=api_key)
 
 # Yaml file
 cur_dir = os.path.dirname(__file__)
